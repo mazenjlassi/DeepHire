20083e789c1e9823027e8725fdc7393f8864974f
Amir <amirouni162@gmail.com>
2025-02-12 18:37:00 +0100

    create all restcontroller

---
+++ src/main/java/org/example/deephire/Repositories/MessageRepository.java
@@ -0,0 +1,9 @@
+package org.example.deephire.Repositories;
+
+import org.example.deephire.models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message,Long> {
+}
